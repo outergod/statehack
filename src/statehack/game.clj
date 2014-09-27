608dc632620b21a5319304d437c11530d4e9fc63
Alexander Kahl <e-user@fsfe.org>
2014-09-27 15:05:52 +0200

    Removed :mode cruft.

--- src/statehack/game.clj
+++ src/statehack/game.clj
@@ -19,8 +19,7 @@ XXXXXXXXXXX")
   (let [{:keys [id] :as player} (player/player 40 18)]
     {:screen scr
      :viewport [0 0]
-     :world [{:mode :world
-              :foundation (render/space 80 24)
+     :world [{:foundation (render/space 80 24)
               :player id
               :receivers [id]
               :entities (util/index-by :id
