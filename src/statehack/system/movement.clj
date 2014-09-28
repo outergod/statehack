d3019c39982c990ed2cf9e84aab71ea63fd02aa4
Alexander Kahl <e-user@fsfe.org>
2014-09-28 18:44:57 +0200

    Refactored.

--- src/statehack/system/movement.clj
+++ src/statehack/system/movement.clj
@@ -12,9 +12,6 @@
 
 (defmulti move #'move-dispatch :hierarchy #'move-hierarchy)
 
-(defn update-position [e x y]
-  (assoc-in e [:position] [x y]))
-
 (defmethod move :player [e game x y]
   (let [state (world/current-world-state game)
         [x y] (util/matrix-add (:position e) [x y])]
@@ -22,6 +19,6 @@
       (if (seq (world/entities-at state [x y]))
         (dialog/message game "You can't, there's an obstacle.")
         (-> game world/dup-world-state
-            (world/update-entity e update-position x y)
+            (world/update-entity-component e :position (constantly [x y]))
             (viewport/center-viewport e)))
       (dialog/message game "Somehow, you can't move here.."))))
