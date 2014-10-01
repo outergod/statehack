236d4f59e4b97ffaf7cff0994f371aae389f0e3d
Alexander Kahl <e-user@fsfe.org>
2014-10-01 12:30:33 +0200

    Nicer crash handling.

--- dev/user.clj
+++ dev/user.clj
@@ -6,9 +6,24 @@
             [statehack.game.world :as world]
             [statehack.system.input :as input]
             [statehack.system.movement :as movement]
-            [statehack.system.door :as door]))
+            [statehack.system.door :as door]
+            [clojure.stacktrace :refer :all]
+            [clojure.pprint :refer :all])
+  (:import [clojure.lang IExceptionInfo]))
 
 (def scr (screen/get-screen :text))
+
+(def crash-state (atom {}))
+
+(defn run []
+  (try (game/run scr)
+       (catch Throwable e
+         (if (instance? IExceptionInfo e)
+           (let [{:keys [state]} (ex-data e)]
+             (println "Crash state available.")
+             (swap! crash-state (constantly state))
+             (throw (.getCause e)))
+           (throw e)))))
 
 (comment
   (game/run scr)
