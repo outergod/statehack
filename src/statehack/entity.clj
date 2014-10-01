184b92b612f5d4aec27c3438568ec34175767522
Alexander Kahl <e-user@fsfe.org>
2014-10-01 15:43:29 +0200

    Components querying.

--- src/statehack/entity.clj
+++ src/statehack/entity.clj
@@ -6,8 +6,12 @@
 (defn entity [& components]
   (apply merge {:id (uuid)} components))
 
+(defn components [e]
+  (set (keys e)))
+
 (defn capable? [e & cs]
-  (set/subset? (set cs) (set (keys e))))
+  (set/subset? (set cs) (components e)))
 
 (defn filter-capable [[& cs] es]
   (filter #(apply capable? % cs) es))
+
