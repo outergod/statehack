7706efa119068cca4dc04d8db79b06540fcbd57d
Alexander Kahl <e-user@fsfe.org>
2014-10-09 13:04:22 +0200

    Center smaller worlds into screen.

--- src/statehack/system/render.clj
+++ src/statehack/system/render.clj
@@ -125,6 +125,24 @@
   ([graphics canvas]
      (put-canvas graphics canvas 0 0)))
 
+(defn center-offset
+  "Calculate the offset needed to center `canvas` into a region of
+  width `w` and height `h`."
+  [canvas [w h]]
+  (let [cw (count (first canvas))
+        ch (count canvas)]
+    [(int (max (/ (- w cw) 2) 0))
+     (int (max (/ (- h ch) 2) 0))]))
+
+(defn fit-in [canvas x0 y0 w h]
+  (let [base (rect :nihil 0 w h)
+        cw (count (first canvas))
+        ch (count canvas)]
+    (apply canvas-blit base (subvec (mapv #(subvec % x0 (min (+ x0 w) cw))
+                                          canvas)
+                                    y0 (min (+ y0 h) ch))
+           (center-offset canvas [w h]))))
+
 (defn- draw-world [game es canvas]
   (let [{:keys [graphics viewport]} game
         {:keys [foundation]} (world/current-world-state game)
@@ -133,7 +151,7 @@
                       (entity-canvas es))
         [x y] viewport
         [w h] (size graphics :world)
-        view (subvec (mapv #(subvec % x (+ x w)) world) y (+ y h))
+        view (fit-in world x y w h)
         [x0 y0] (position graphics :world)]
     (canvas-blit canvas view x0 y0)))
 
@@ -164,11 +182,13 @@
 (defn draw-cursor [game es]
   (let [cursor (first (filter #(= (-> % :mobile :type) :cursor) es))
         {:keys [screen graphics viewport]} game
+        {:keys [foundation]} (world/current-world-state game)
         section (receiver-section game)
         [x y] (util/matrix-add (position graphics section)
                                (:position cursor))
         [x y] (if (= section :world)
-                (util/matrix-subtract [x y] viewport)
+                (util/matrix-add (util/matrix-subtract [x y] viewport)
+                                 (center-offset foundation (size graphics :world)))
                 [x y])]
     (screen/move-cursor screen x y)))
 
