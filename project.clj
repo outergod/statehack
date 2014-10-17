e5a9ae837a5f6f852039dc0306ce8c6a9da70519
Alexander Kahl <e-user@fsfe.org>
2014-10-17 17:37:58 +0200

    Integrated clj-audio, original sshock sounds.

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojars.wmealing/clj-audio "0.2.0-SNAPSHOT"]
                  ;[clojure-lanterna "0.9.4"]
                  ;[org.clojars.folcon/clojure-lanterna "0.9.5"]
                  [com.googlecode.lanterna/lanterna "3.0.0-alpha3"]]
--- src/statehack/system/sound.clj
+++ src/statehack/system/sound.clj
@@ -1,96 +1,30 @@
 (ns statehack.system.sound
-  (:require [statehack.system.world :as world]
-            [statehack.util :as util]
-            [clojure.java.io :as io])
+  (:require [clojure.java.io :as io]
+            [clj-audio.core :as audio])
   (:import [clojure.lang ISeq Seqable Sequential]
            [javax.sound.sampled AudioSystem DataLine$Info Clip]))
 
-(defprotocol Closeable
-  (close [this]))
-
-(deftype CloseableSeq [^ISeq coll]
-  ISeq
-  (first [_] (.first coll))
-  (next [_] (.next coll))
-  (more [_] (.more coll))
-  (cons [_ o] (.cons coll o))
-
-  Sequential
-  Seqable
-  (seq [_] coll)
-  
-  Closeable
-  (close [_] (doseq [e coll] (.close e))))
-
-(def sound-resources
+(def resources
   {:gatling-wind-up "gatling-wind-up.wav"
    :gatling-wind-down "gatling-wind-down.wav"
    :gatling-wind-loop "gatling-wind-loop.wav"
-   :punch-02 "punch-02.wav"})
+   :punch-02 "punch-02.wav"
+   :man-dying "man-dying.wav"
+   :door "sshock/00206.wav"
+   :airlock-door "sshock/00204.wav"
+   :blast-door "sshock/00268.wav"
+   :ion-rifle "sshock/00296.wav"
+   :gauss-rifle "sshock/00230.wav"
+   :pistol "sshock/00240.wav"
+   :magnum-2100 "sshock/00241.wav"})
 
-(defn resource [name]
+(defn load-resource [name]
   (or (io/resource (str "sounds/" name))
       (throw (ex-info (str "No such sound resource found") {:name name}))))
 
-(defn load-sound [res]
-  (let [snd (AudioSystem/getAudioInputStream res)
-        clip (->> snd .getFormat (DataLine$Info. Clip) AudioSystem/getLine)]
-    (doto clip (.open snd))))
-
-(defn sound-format [snd] (.getFormat snd))
-
-(defn sound [name]
-  (load-sound (resource (sound-resources name))))
-
-(def sounds
-  (into {} (map (fn [[k v]] [k (load-sound (resource v))]) sound-resources)))
-
-(defn clip-length [clip]
-  (float (/ (.getMicrosecondLength clip) (Math/pow 10 6))))
-
-(defn multi-sound [name time]
-  (let [res (resource (sound-resources name))
-        s (load-sound res)
-        n (Math/ceil (/ (clip-length s)
-                        (/ time 1000)))]
-    (CloseableSeq. (cons s (repeatedly n #(load-sound res))))))
-
-(defn ready-sound
-  ([sound offset]
-     (doto sound .stop (.setFramePosition offset)))
-  ([sound]
-     (ready-sound sound 0)))
-
-(defn play-sound [sound]
-  (doto sound .start))
-
-(defn play-multi-sound [sound n time]
-  (dotimes [x n]
-    (let [s (nth sound (mod x (count sound)))]
-      (-> s ready-sound play-sound)
-      (Thread/sleep time))))
-
-(defn wait-sound
-  ([sound n]
-     (Thread/sleep (- (* (clip-length sound) 1000 n) 100))) ; - 100 -> seamless
-  ([sound]
-     (wait-sound sound 1)))
-
-(defn hz->mseconds [hz]
-  (int (/ 1000 hz)))
+(defn load-stream [name]
+  (-> name resources load-resource audio/->stream))
 
 (defn play [name]
-  (-> name sounds ready-sound play-sound))
-
-(comment
-  (let [t (sound/hz->mseconds 10)]
-    (with-open [up (sound/sound :gatling-wind-up)
-                down (sound/sound :gatling-wind-down)
-                loop (sound/multi-sound :gatling-wind-loop t)]
-      (-> up sound/ready-sound sound/play-sound sound/wait-sound)
-      (dotimes [n 20]
-        (let [s (nth loop (mod n (count loop)))]
-          (-> s sound/ready-sound sound/play-sound)
-          (Thread/sleep t)))
-      (-> down sound/ready-sound sound/play-sound sound/wait-sound)
-      (println (count loop)))))
+  (with-open [s (load-stream name)]
+    (audio/play s)))
