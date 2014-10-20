b477340297b89677b700ef8542b8644e0fb0c81a
Alexander Kahl <e-user@fsfe.org>
2014-10-20 12:11:06 +0200

    Always use vectors for matrix arithmetic operations.

--- src/statehack/util.clj
+++ src/statehack/util.clj
@@ -3,14 +3,14 @@
 (defn matrix-add
   ([x] x)
   ([x y]
-     (map + x y))
+     (mapv + x y))
   ([x y & more]
      (reduce matrix-add (matrix-add x y) more)))
 
 (defn matrix-subtract
   ([x] x)
   ([x y]
-     (map - x y))
+     (mapv - x y))
   ([x y & more]
      (reduce matrix-subtract (matrix-subtract x y) more)))
 
