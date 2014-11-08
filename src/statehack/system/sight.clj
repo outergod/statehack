f4bdaf3859ae073452ca506e709f7666361c7318
Alexander Kahl <e-user@fsfe.org>
2014-11-08 12:14:34 +0100

    Different sights, including new omniscience, added.

--- src/statehack/system/sight.clj
+++ src/statehack/system/sight.clj
@@ -4,7 +4,10 @@
             [statehack.system.world :as world]
             [statehack.system.levels :as levels]
             [statehack.entity :as entity]
+            [statehack.system.levels :as levels]
             [statehack.util :as util]))
+
+;;; opaque? multimethod
 
 (def opaque?-hierarchy "Hierarchy for `opaque?`" (make-hierarchy))
 
@@ -28,19 +31,50 @@
 
 (defmethod opaque? :door [e] (not (:open e)))
 
-(defn filter-opaques [es]
+(defn filter-opaques
+  "Filter opaques from `es`"
+  [es]
   (filter opaque? es))
 
-(defn visible-mask
-  "Visible coordinates for sighted entity `e`."
+;;; visible-mask multimethod
+
+(def visible-mask-hierarchy "Hierarchy for `visible-mask`" (make-hierarchy))
+
+(defn derive-visible-mask
+  "Derive for `visible-mask-hierarchy`"
+  [tag parent]
+  (alter-var-root #'visible-mask-hierarchy derive tag parent))
+
+(defn visible-mask-dispatch
+  "Dispatch for `visible-mask`"
   [game e]
-  {:pre [(:sight e) (:position e) (:floor e)]}
+  (-> e :sight :type))
+
+(defmulti visible-mask
+  "Is entity visible-mask"
+  {:arglists '([game e])}
+  #'visible-mask-dispatch :hierarchy #'visible-mask-hierarchy)
+
+(defn- visible-mask-common
+  "Shared implementation of `visible-mask` for organic and sensoric sighted entities"
+  [game e]
   (let [[x y] (:position e)
         r (-> e :sight :distance)
         ps (set (map :position (filter-opaques (levels/on-floor (:floor e) (entity/filter-capable [:position :floor] (vals (world/entities game)))))))]
     (conj (set (mapcat (partial util/take-while-including (complement ps))
                        (algebra/visible-lines [x y] r)))
           [x y])))
+
+(defmethod visible-mask :eyes [game e]
+  (visible-mask-common game e))
+
+(defmethod visible-mask :sensors [game e]
+  (visible-mask-common game e))
+
+(defmethod visible-mask :omniscience [game e]
+  (let [{:keys [foundation]} (levels/entity-floor game e)
+        [w h] foundation]
+    (set (for [x (range w) y (range h)] [x y]))))
 
 (defn visible-entities
   "Visible entities on `floor` within `mask`"
