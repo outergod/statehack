fa4a1648e451949f2255d7d6fe50a28fa48f5946
Alexander Kahl <ak@sodosopa.io>
2017-01-31 10:22:15 +0100

    Update to Clojure 1.8.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/e-user/statehack"
   :license {:name "GNU General Public License v3 (or later)"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.priority-map "0.0.5"]
                  [org.clojars.wmealing/clj-audio "0.2.0-SNAPSHOT"]
                  [com.googlecode.lanterna/lanterna "3.0.0-alpha3"]
