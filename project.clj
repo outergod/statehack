e04edd1f3362451756de36c5f9e12b807a83f042
Alexander Kahl <ak@sodosopa.io>
2017-02-03 18:47:02 +0100

    Update to clojure-1.9.0-alpha14
    And aleph-0.4.2-alpha12.

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/e-user/statehack"
   :license {:name "GNU General Public License v3 (or later)"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [org.clojure/data.priority-map "0.0.5"]
                  [org.clojars.wmealing/clj-audio "0.2.0-SNAPSHOT"]
                  [com.googlecode.lanterna/lanterna "3.0.0-alpha3"]
                  [com.googlecode.soundlibs/vorbisspi "1.0.3-1"]
                  [simple-time "0.2.0"]
-                 [aleph "0.4.1"]
+                 [aleph "0.4.2-alpha12"]
                  [compojure "1.4.0"]
                  [javax.servlet/servlet-api "2.5"]]
   :target-path "target/%s"
